4a7036d65af34d85686c2cd7d452c0d29d1f5553
tanguybmx <tanguyxp@live.fr>
2019-03-25 13:06:54 +0100

    connect db

--- web/INC/connectionDB.php
+++ web/INC/connectionDB.php
@@ -5,3 +5,15 @@
  * Date: 25-03-19
  * Time: 10:03
  */
+$servername = "localhost";
+$username = "root";
+$password = "root";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+echo "Connected successfully";
